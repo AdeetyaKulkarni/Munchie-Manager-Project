7475e782e5414c728835fcd961d2066c4f05fd62
Adeetya Kulkarni <adeetya.kulkarni@ttu.edu>
2020-09-19 18:34:21 -0500

    Environment init

---
+++ proj/src/test/java/com/MunchieManager/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MunchieManager.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
